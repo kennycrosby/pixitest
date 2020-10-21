1d80cbf90e458c5f5f8fe64789785dad93ae5eb5
Kenny <ken.crosby65@gmail.com>
2020-10-21 17:13:37 -0400

    Testing webp

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,13 @@ import { ScrollTrigger } from 'gsap/all';
 import * as PIXI from 'pixi.js';
 
 import maskTest from '../assets/masktest.svg';
+import mask from '../assets/mask.png';
+
+import tbg from '../assets/tbg.png';
+// import tgirls from '../assets/tgirls.png';
+import tgirls from '../assets/scene_selfiegirls.webp';
+import tmask from '../assets/tmask.png';
+
 import bg from '../assets/scene-1-bg.jpg';
 import clouds1 from '../assets/clouds-large.png';
 import girls from '../assets/Scene_SelfieGirls.png';
@@ -59,17 +66,14 @@ const state = {
 };
 
 const ScrollScene = ({ w, h }) => {
-  const scrollSceneRef = useRef(null);
+  const mainScrollSceneRef = useRef(null);
+  const subSceneRef = useRef(null);
   const girlSpriteRef = useRef(null);
   const maskRef = useRef(null);
+  const bgMaskRef = useRef(null);
   const bgRef = useRef(null);
 
   const cloudRefs = useRef([]);
-  const cloud1SpriteRef = useRef(null);
-  const cloud2SpriteRef = useRef(null);
-  const cloud3SpriteRef = useRef(null);
-  const cloud4SpriteRef = useRef(null);
-
   const [motion, update] = useReducer(reducer);
   const iter = useRef(0);
 
@@ -77,53 +81,58 @@ const ScrollScene = ({ w, h }) => {
   useEffect(() => {
     // cloud1SpriteRef.current.blendMode = PIXI.BLEND_MODES.MULTIPLY;
 
-    // girlSpriteRef.current.mask = bgRef.current;
+    subSceneRef.current.pivot.set();
+
+    console.log('ScrollScene -> mainScrollSceneRef WIDTH', mainScrollSceneRef.current.width);
+    console.log('ScrollScene -> mainScrollSceneRef HEIGHT', mainScrollSceneRef.current.height);
+    console.log('ScrollScene -> subSceneRef WIDTH', subSceneRef.current.width);
+    console.log('ScrollScene -> subSceneRef HEIGHT', subSceneRef.current.height);
+
+    // maskRef.current.mask = girlSpriteRef.current;
+    // maskRef.current.mask = bgRef.current;
+    bgRef.current.mask = bgMaskRef.current;
 
     gsap.registerPlugin(ScrollTrigger);
 
-    setTimeout(() => {
-      const tl = gsap.timeline({
-        scrollTrigger: {
-          // trigger: listRef.current,
-          // start: 'top 100%',
-          // markers: true,
-          toggleActions: 'play none none reset', // onEnter, onLeave, onEnterBack, onLeaveBack
-          // Options: "play", "pause", "resume", "reset", "restart", "complete", "reverse", and "none".
-          scrub: true
-        }
-      });
+    // setTimeout(() => {
+    //   const tl = gsap.timeline({
+    //     scrollTrigger: {
+    //       // trigger: listRef.current,
+    //       // start: 'top 100%',
+    //       // markers: true,
+    //       toggleActions: 'play none none reset', // onEnter, onLeave, onEnterBack, onLeaveBack
+    //       // Options: "play", "pause", "resume", "reset", "restart", "complete", "reverse", and "none".
+    //       scrub: true
+    //     }
+    //   });
 
-      tl.to(scrollSceneRef.current.scale, {
-        x: 120,
-        y: 120,
-        duration: 1,
-        ease: 'power2.in'
-      }, 'start').to(girlSpriteRef.current, {
-        x: 100,
-        y: 90,
-        duration: 1,
-        ease: 'power2.out'
-      }, '-=1')
-    }, 1500);
+    //   tl.to(
+    //     mainScrollSceneRef.current.scale,
+    //     {
+    //       x: 120,
+    //       y: 120,
+    //       duration: 1,
+    //       ease: 'power2.in'
+    //     },
+    //     'start'
+    //   ).to(
+    //     mainScrollSceneRef.current,
+    //     {
+    //       x: 600,
+    //       y: 690,
+    //       duration: 1,
+    //       ease: 'power2.out'
+    //     },
+    //     '-=1'
+    //   );
+    // }, 1500);
   }, []);
 
   // cloud animation
   useTick((delta) => {
     const i = (iter.current += 0.1);
-    // const i = 0;
-
-    // const sprite = cloud1SpriteRef.current;
-    // // console.log("ScrollScene -> sprite", sprite)
-    // const currentX = sprite.transform.position._x;
-    // const currentY = sprite.transform.position._y;
-
     let newSceneState = sceneState;
-    // console.log("ScrollScene -> sceneState", sceneState)
-    // console.log("ScrollScene -> cloudRefs", cloudRefs)
-    // console.log("ScrollScene -> cloudRefs.length", cloudRefs.length)
-
-    if(cloudRefs && cloudRefs.current) {
-
+    if (cloudRefs && cloudRefs.current) {
       cloudRefs.current.map((cloud, i) => {
         let animationSpeed = 1 - cloud.scale.x + 0.1;
 
@@ -133,18 +142,14 @@ const ScrollScene = ({ w, h }) => {
 
         newSceneState.clouds[i] = {
           ...sceneState.clouds[i],
-          x: currentX >= w / 2 ? -w / 2 - sprite.width : currentX + animationSpeed,
+          x: currentX >= w / 2 ? -w / 2 - sprite.width : currentX + animationSpeed
         };
       });
-
-      // console.log("ScrollScene -> newSceneState", newSceneState)
-
       update({
         type: 'update',
         data: { ...newSceneState }
       });
     }
-
 
     // update({
     //   type: 'update',
@@ -160,33 +165,55 @@ const ScrollScene = ({ w, h }) => {
     // });
   });
 
+  const sceneWidth = 1440;
+  const sceneHeight = 1024;
+
+  const sceneAspect = sceneWidth / sceneHeight;
+
   return (
-    <Container position={[0, 0]} ref={scrollSceneRef} x={w / 2} y={h / 2} sortableChildren={true}>
-      {/* <Sprite ref={maskRef} image={maskTest} x={0} y={0} scale={{ x: 5.5, y: 5.5 }} anchor={0.5} /> */}
+    <Container
+      position={[w / 2, h / 2]}
+      scale={
+        w / h > sceneAspect ? { x: w / sceneWidth, y: w / sceneWidth } : { x: h / sceneHeight, y: h / sceneHeight }
+      }
+      sortableChildren={true}
+    >
+      {/* <Sprite ref={maskRef} image={mask} scale={{ x: 1, y: 1 }} anchor={0.5} zIndex={6} />
+      <Sprite ref={girlSpriteRef} image={girls} mask={maskRef.current} scale={{ x: 0.5, y: 0.5 }} anchor={0.5} zIndex={5} /> */}
       {/* <Sprite ref={bgRef} image={bg} x={0} y={0} scale={{ x: 1.5, y: 1.5 }} anchor={0.5} zIndex={0.1} /> */}
-      <Sprite ref={girlSpriteRef} image={girls} scale={{ x: 0.5, y: 0.5 }} anchor={0.5} zIndex={5} />
-      {/* <Sprite ref={cloud1SpriteRef} image={clouds1} x={-950} y={-950} scale={{ x: 1, y: 1 }} zIndex={0.6} /> */}
 
-      {sceneState.clouds.map((cloud, i) => {
-        return (
-          <Sprite
-            key={i}
-            ref={(el) => (cloudRefs.current[i] = el)}
-            image={clouds1}
-            x={cloud.x}
-            y={cloud.y}
-            scale={cloud.scale}
-            zIndex={6}
-            anchor={0}
-          />
-        );
-      })}
+      <Container sortableChildren={true} ref={mainScrollSceneRef}>
+        <Container ref={subSceneRef} sortableChildren={true}>
+          <Sprite ref={bgRef} image={tbg} scale={{ x: 1, y: 1 }} anchor={0.5} zIndex={0.1} />
+          <Sprite ref={bgMaskRef} image={tmask} scale={{ x: 1, y: 1 }} anchor={0.5} zIndex={0.2} />
+          <Container ref={girlSpriteRef} sortableChildren={true}>
+            <Sprite
+              image={tgirls}
+              mask={maskRef.current}
+              scale={{ x: 1440 / 7262, y: 1440 / 7262 }}
+              anchor={0.5}
+              zIndex={0.5}
+            />
+          </Container>
+          <Sprite ref={maskRef} image={tmask} scale={{ x: 1, y: 1 }} anchor={0.5} zIndex={0.6} />
+          {/* <Sprite ref={maskRef} image={maskTest} scale={{ x: 1, y: 1 }} anchor={0.5} zIndex={6} /> */}
+        </Container>
 
-
-      {/* <Sprite bgRef={maskRef} image={maskTest} x={0} y={0} scale={{ x: 5.5, y: 5.5 }} anchor={0.5} /> */}
-      {/* <Sprite ref={cloud2SpriteRef} image={clouds1} x={-800} y={-500} scale={{x: 0.1, y: 0.1}} anchor={0.5} {...motion} />
-      <Sprite ref={cloud3SpriteRef} image={clouds1} x={-800} y={-500} scale={{x: 0.6, y: 0.6}} anchor={0.5} {...motion} />
-      <Sprite ref={cloud4SpriteRef} image={clouds1} x={-800} y={-500} scale={{x: 0.6, y: 0.6}} anchor={0.5} {...motion} /> */}
+        {sceneState.clouds.map((cloud, i) => {
+          return (
+            <Sprite
+              key={i}
+              ref={(el) => (cloudRefs.current[i] = el)}
+              image={clouds1}
+              x={cloud.x}
+              y={cloud.y}
+              scale={cloud.scale}
+              zIndex={6}
+              anchor={0}
+            />
+          );
+        })}
+      </Container>
     </Container>
   );
 };
