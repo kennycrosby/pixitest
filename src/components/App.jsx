8ec54956262d0ff4f4c85145573ecde8eeff7284
Mike Manh <github.123@vteklabs.com>
2020-10-22 12:02:50 -0400

    updated the code to have a cyclical page change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -22,83 +22,35 @@ import bg from '../assets/scene-1-bg.jpg';
 import clouds1 from '../assets/clouds-large.png';
 import selfieGirls from '../assets/Scene_SelfieGirls.png';
 
+import sceneImage1Path from '../assets/testscene1.png';
+import sceneImage2Path from '../assets/testscene2.png';
+import sceneImage3Path from '../assets/testscene3.png';
+
+const sceneImageArray = [sceneImage1Path,sceneImage2Path,sceneImage3Path];
+
+
+
 // The reducer
 const reducer = (_, { data }) => data;
 
-const sceneState = {
-  clouds: [
-    {
-      x: -950,
-      y: -950,
-      scale: { x: 1, y: 1 },
-      zIndex: 3,
-      spriteImage: clouds1,
-      anchor: 0.5,
-      animationDirection: 'LTR'
-    },
-    {
-      x: 150,
-      y: 350,
-      scale: { x: 0.5, y: 0.5 },
-      zIndex: 3,
-      spriteImage: clouds1,
-      anchor: 0.5,
-      animationDirection: 'RTL'
-    },
-    {
-      x: -450,
-      y: -550,
-      scale: { x: 0.8, y: 0.8 },
-      zIndex: 3,
-      spriteImage: clouds1,
-      anchor: 0.5,
-      animationDirection: 'LTR'
-    }
-  ],
-  girls: {
-    distance: 0
-  }
-};
-
-// PIXI must be exposed for PIXI devtools to work
 window.PIXI = PIXI;
 
-const state = {
-  sections: 3,
-  pages: 3,
-  zoom: 75,
-  top: createRef()
-};
 
 const ScrollScene = ({ w, h }) => {
-  const mainScrollSceneRef = useRef(null);
   const subSceneRef = useRef(null);
-  const subScene2Ref = useRef(null);
-  const selfieGirlsSpriteRef = useRef(null);
-  const threeGirlsRef = useRef(null);
-  const maskRef = useRef(null);
-  const bgMaskRef = useRef(null);
-  const bgRef = useRef(null);
 
-  const cloudRefs = useRef([]);
-  const [motion, update] = useReducer(reducer);
-  const iter = useRef(0);
+
+  const [pageProgress, setPageProgress] = useState(0); //continuous between 0-3
+  const [curProgress, setCurProgress] = useState(0); //continuous between 0-1
+  const [curPage, setCurPage] = useState(0); //integral between 0-3
+  const [nextPage, setNextPage] = useState(1);
+  
 
   // scroll animation
   useEffect(() => {
     // cloud1SpriteRef.current.blendMode = PIXI.BLEND_MODES.MULTIPLY;
 
-    subSceneRef.current.pivot.set();
-
-    console.log('ScrollScene -> mainScrollSceneRef WIDTH', mainScrollSceneRef.current.width);
-    console.log('ScrollScene -> mainScrollSceneRef HEIGHT', mainScrollSceneRef.current.height);
-    console.log('ScrollScene -> subSceneRef WIDTH', subSceneRef.current.width);
-    console.log('ScrollScene -> subSceneRef HEIGHT', subSceneRef.current.height);
-
-    // maskRef.current.mask = girlSpriteRef.current;
-    // maskRef.current.mask = bgRef.current;
-    //bgRef.current.mask = bgMaskRef.current;
-
+    
 
     gsap.registerPlugin(ScrollTrigger);
 
@@ -112,68 +64,50 @@ const ScrollScene = ({ w, h }) => {
           // markers: true,
           toggleActions: 'play none none reset', // onEnter, onLeave, onEnterBack, onLeaveBack
           // Options: "play", "pause", "resume", "reset", "restart", "complete", "reverse", and "none".
-          scrub: true
+          scrub: true,
+          onUpdate: ({progress,direction,isActive})=>{
+            console.log( "onUpdate:", progress, direction, isActive);
+            var tempPageProgress = progress * 4;
+            setPageProgress( tempPageProgress );
+            setCurProgress( tempPageProgress % 1);
+            var tempCurPage = Math.floor( progress*4);
+            setCurPage( tempCurPage % 3 );
+            setNextPage( ( tempCurPage+1) %3 ); //in case the cur page is 3 and the next page is 0
+
+          }
         }
+        
       });
 
-      tl.to(
-        subSceneRef.current.scale,
-        {
-          x: 120,
-          y: 120,
-          duration: 1,
-          ease: 'power2.in'
-        },
-        'start'
-      ).to(
-        subSceneRef.current,
-        {
-          x: 8000, //the reason this value is so high is because it is being scaled to 120,
-          y: 12000, // so to zoom in on some point x and y, you have to set the x to 120*x, 120*y
-          duration: 1,
-          ease: 'power2.in' //and it has to have the same easing curve for both so they align
-        },
-        '-=1'
-      );
+      // tl.to(
+      //   subSceneRef.current.scale,
+      //   {
+      //     x: 120,
+      //     y: 120,
+      //     duration: 1,
+      //     ease: 'power2.in'
+      //   },
+      //   'start'
+      // ).to(
+      //   subSceneRef.current,
+      //   {
+      //     x: 8000, //the reason this value is so high is because it is being scaled to 120,
+      //     y: 12000, // so to zoom in on some point x and y, you have to set the x to 120*x, 120*y
+      //     duration: 1,
+      //     ease: 'power2.in' //and it has to have the same easing curve for both so they align
+      //   },
+      //   '-=1'
+      // );
     }, 1500);
   }, []);
 
-  // cloud animation
-  useTick((delta) => {
-    const i = (iter.current += 0.1);
-    let newSceneState = sceneState;
-    if (cloudRefs && cloudRefs.current) {
-      cloudRefs.current.map((cloud, i) => {
-        let animationSpeed = 1 - cloud.scale.x + 0.1;
 
-        const sprite = cloud;
-        const currentX = sprite.transform.position._x;
-        const currentY = sprite.transform.position._y;
+  useEffect( ()=>{
 
-        newSceneState.clouds[i] = {
-          ...sceneState.clouds[i],
-          x: currentX >= w / 2 ? -w / 2 - sprite.width : currentX + animationSpeed
-        };
-      });
-      update({
-        type: 'update',
-        data: { ...newSceneState }
-      });
-    }
+  }, [pageProgress]);
 
-    // update({
-    //   type: 'update',
-    //   data: {
-    //     x: currentX >= w / 2 ? -w / 2 - sprite.width : currentX + animationSpeed
-    //     // x: currentX + animationSpeed
-    //     // x: Math.sin(i) * 10, // oscilate
-    //     // y: Math.sin(i / 1.5) * 100,
-    //     // rotation: Math.sin(i) * Math.PI,
-    //     // anchor: Math.sin(i / 2),
-    //     // scale: new PIXI.Point(currentX + i, currentY + i)
-    //   }
-    // });
-  });
+
+  
 
   const sceneWidth = 1440;
   const sceneHeight = 1024;
@@ -188,60 +122,22 @@ const ScrollScene = ({ w, h }) => {
       }
       sortableChildren={true}
     >
-      {/* <Sprite ref={maskRef} image={mask} scale={{ x: 1, y: 1 }} anchor={0.5} zIndex={6} />
-      <Sprite ref={girlSpriteRef} image={girls} mask={maskRef.current} scale={{ x: 0.5, y: 0.5 }} anchor={0.5} zIndex={5} /> */}
-      {/* <Sprite ref={bgRef} image={bg} x={0} y={0} scale={{ x: 1.5, y: 1.5 }} anchor={0.5} zIndex={0.1} /> */}
-
-      <Container sortableChildren={true} ref={mainScrollSceneRef}>
-        <Container ref={subSceneRef} zIndex={7}>
-          {/* <Sprite ref={bgRef} image={tbg} scale={{ x: 1, y: 1 }} anchor={0.5} /> */}
-          {/* <Sprite ref={bgMaskRef} image={tmask} scale={{ x: 1, y: 1 }} anchor={0.5} /> */}
-          <Container ref={selfieGirlsSpriteRef} >
-            <Sprite //can be swapped out with the normal sprite at normal scale if they use a non-webp capable browser  
-              image={selfieGirls}
+      <Sprite //can be swapped out with the normal sprite at normal scale if they use a non-webp capable browser  
+              image={sceneImageArray[curPage]}
               
-              scale={[1440 / 7262, 1440 / 7262]}
-              anchor={0.5}
-              zIndex={0.5}
-              position={[-21, -80]}
-            />
-          </Container>
-          <Sprite //does the z index of the mask matter?
-            ref={maskRef} image={phoneMask} scale={[1440 / 7262, 1440 / 7262]} anchor={0.5} zIndex={9} position={[-21, -80]} />
-
-          {/* <Sprite ref={maskRef} image={maskTest} scale={{ x: 1, y: 1 }} anchor={0.5} zIndex={6} /> */}
-        </Container>
-        <Container ref={subScene2Ref} zIndex={9} mask={maskRef.current}>          
-          <Container ref={threeGirlsRef} >
-            <Sprite //can be swapped out with the normal sprite at normal scale if they use a non-webp capable browser  
-              image={threeGirlsFile}
-              
-              scale={[1440 / 2928, 1440 / 2928]}
-              anchor={0.5}
-              zIndex={0.5}
-              position={[-21, -80]}
-            />
-          </Container>
           
-
-          {/* <Sprite ref={maskRef} image={maskTest} scale={{ x: 1, y: 1 }} anchor={0.5} zIndex={6} /> */}
-        </Container>
-
-        {sceneState.clouds.map((cloud, i) => {
-          return (
-            <Sprite
-              key={i}
-              ref={(el) => (cloudRefs.current[i] = el)}
-              image={clouds1}
-              x={cloud.x}
-              y={cloud.y}
-              scale={cloud.scale}
-              zIndex={10}
-              anchor={0}
+              anchor={0.5}
+              zIndex={2}
+              scale={1+(curProgress *5)}
             />
-          );
-        })}
-      </Container>
+        <Sprite //can be swapped out with the normal sprite at normal scale if they use a non-webp capable browser  
+          image={sceneImageArray[nextPage]}
+          
+          
+          anchor={0.5}
+          zIndex={1}
+          // position={[-21, -80]}
+        />
     </Container>
   );
 };
@@ -263,14 +159,9 @@ const Size = ({ children }) => {
 
 export const App = () => {
   const scrollArea = useRef();
-  const onScroll = (e) => (state.top.current = e.target.scrollTop);
+  
 
-  useEffect(() => void onScroll({ target: scrollArea.current }), []);
-
-  useEffect(() => {
-    console.log('state.top.current', state.top.current);
-  }, [state.top.current]);
-
+  
   return (
     <>
       <Stage
@@ -284,8 +175,8 @@ export const App = () => {
       >
         <Size>{({ width, height }) => <ScrollScene w={width} h={height} />}</Size>
       </Stage>
-      <div ref={scrollArea} onScroll={onScroll}>
-        <div style={{ height: `${state.pages * 100}vh` }} />
+      <div ref={scrollArea} >
+        <div style={{ height: `${3 * 500}vh` }} />
       </div>
     </>
   );
