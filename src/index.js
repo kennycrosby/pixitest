29837dc83512ec1a7d15c6a4f6f9fba926f1c588
Kenny <ken.crosby65@gmail.com>
2020-10-23 21:44:35 -0400

    Adding snapping to the gsap timeline, tried new lottie files but they no workie

--- src/index.js
+++ src/index.js
@@ -18,6 +18,8 @@ let target = '#app';
 let duration = 4;
 let speed = 'slow';
 
+gsap.registerPlugin(ScrollTrigger);
+
 const ScrollLottie = () => {
   let anim = lottie.loadAnimation({
     container: document.querySelector(target),
@@ -27,43 +29,128 @@ const ScrollLottie = () => {
     animationData: data,
     rendererSettings: {
       // context: canvasContext, // the canvas context, only support "2d" context
-      preserveAspectRatio: 'xMinYMin slice', // Supports the same options as the svg element's preserveAspectRatio property
-      clearCanvas: false,
-      progressiveLoad: false, // Boolean, only svg renderer, loads dom elements when needed. Might speed up initialization for large number of elements.
-      hideOnTransparent: true, //Boolean, only svg renderer, hides elements when opacity reaches 0 (defaults to true)
+      // preserveAspectRatio: 'xMinYMin slice', // Supports the same options as the svg element's preserveAspectRatio property
+      // clearCanvas: false,
+      // progressiveLoad: false, // Boolean, only svg renderer, loads dom elements when needed. Might speed up initialization for large number of elements.
+      // hideOnTransparent: true, //Boolean, only svg renderer, hides elements when opacity reaches 0 (defaults to true)
       className: 'scene1bodymovin',
       id: 'scene1bodymovinId'
     },
     onComplete: () => {}
   });
 
-  let timeObj = { currentFrame: 0 };
-  let endString =
-    speed === 'slow' ? '+=2000' : speed === 'medium' ? '+=1000' : speed === undefined ? '+=1250' : '+=500';
+  // let timeObj = { currentFrame: 0 };
+  // let endString =
+  //   speed === 'slow' ? '+=2000' : speed === 'medium' ? '+=1000' : speed === undefined ? '+=1250' : '+=500';
 
-  ScrollTrigger.create({
-    trigger: target,
-    scrub: true,
-    pin: true,
-    start: 'top top',
-    end: endString,
-    onUpdate: (self) => {
-      if (duration) {
-        gsap.to(timeObj, {
-          duration: duration,
-          currentFrame: Math.floor(self.progress * (anim.totalFrames - 1)),
-          onUpdate: () => {
-            anim.goToAndStop(timeObj.currentFrame, true);
+  // ScrollTrigger.create({
+  //   trigger: target,
+  //   scrub: true,
+  //   pin: true,
+  //   start: 'top top',
+  //   end: endString,
+  //   onUpdate: (self) => {
+  //     if (duration) {
+  //       gsap.to(timeObj, {
+  //         duration: duration,
+  //         currentFrame: Math.floor(self.progress * (anim.totalFrames - 1)),
+  //         onUpdate: () => {
+  //           anim.goToAndStop(timeObj.currentFrame, true);
+  //         },
+  //         ease: 'expo'
+  //       });
+  //     } else {
+  //       anim.goToAndStop(self.progress * (anim.totalFrames - 1), true);
+  //     }
+  //   }
+  // });
+
+  anim.addEventListener('data_ready', () => {
+  console.log("ScrollLottie -> config_ready")
+
+  });
+  anim.addEventListener('config_ready', () => {
+  console.log("ScrollLottie -> config_ready")
+
+  });
+  anim.addEventListener('DOMLoaded', () => {
+    console.log("ScrollLottie -> DOMLoaded")
+
+    setTimeout(() => {
+      let timeObj = { currentFrame: 0 };
+      let endString =
+        speed === 'slow' ? '+=2000' : speed === 'medium' ? '+=1000' : speed === undefined ? '+=1250' : '+=500';
+      const tl = gsap.timeline({
+        onUpdate: (e) => {
+          console.log('ScrollLottie -> e', e);
+        },
+        onComplete: (e) => {
+          console.log('DONE', e);
+        },
+
+        scrollTrigger: {
+          trigger: '#app',
+          pin: true,
+          start: 'top top',
+          end: endString,
+          // start: 'top 100%',
+          // markers: true,
+          // toggleActions: 'play none none reset', // onEnter, onLeave, onEnterBack, onLeaveBack
+          // Options: "play", "pause", "resume", "reset", "restart", "complete", "reverse", and "none".
+          scrub: true,
+          snap: {
+            snapTo: 'labels', // snap to the closest label in the timeline
+            duration: { min: 0.1, max: 2 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+            // delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+            ease: 'none' // the ease of the snap animation ("power3" by default)
+          }
+        }
+      });
+
+      tl.addLabel('start')
+        .to(timeObj, {
+          duration: 2,
+          // currentFrame: Math.floor(tl.progress() * (anim.totalFrames/2 - 1)),
+          currentFrame: anim.totalFrames/2 - 1,
+          onUpdate: (self) => {
+            console.log("ScrollLottie -> tl.progress()", tl.progress())
+            // anim.goToAndStop(Math.round(timeObj.currentFrame), true);
+
+            gsap.to(timeObj, {
+              duration: duration,
+              currentFrame: Math.floor(tl.progress() * (anim.totalFrames/2 - 1)),
+              onUpdate: () => {
+                anim.goToAndStop(timeObj.currentFrame, true);
+              },
+              ease: 'expo'
+            });
+          },
+          ease: 'none'
+        })
+        .addLabel('middle')
+        .to(timeObj, {
+          duration: 2,
+          // currentFrame: Math.floor(tl.progress() * (anim.totalFrames - 1)),
+          currentFrame: anim.totalFrames - 1,
+          onUpdate: (self) => {
+            console.log("ScrollLottie -> tl.progress()", tl.progress())
+            // anim.goToAndStop(Math.round(timeObj.currentFrame), true);
+
+            gsap.to(timeObj, {
+              duration: duration,
+              currentFrame: Math.floor(tl.progress() * (anim.totalFrames - 1)),
+              onUpdate: () => {
+                anim.goToAndStop(timeObj.currentFrame, true);
+              },
+              ease: 'expo'
+            });
           },
           ease: 'expo'
         });
-      } else {
-        anim.goToAndStop(self.progress * (anim.totalFrames - 1), true);
-      }
-    }
-  });
 
-  anim.addEventListener('DOMLoaded', () => {
+      // tl.play();
+    }, 1500);
+
     resizeAnimation();
   });
 
@@ -75,8 +162,6 @@ const ScrollLottie = () => {
     }, 150);
   });
 };
-
-gsap.registerPlugin(ScrollTrigger);
 
 ScrollLottie();
 
